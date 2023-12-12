2eaa82db1eddd232ac251b65c4fa73ee89a5a507
Rick South <rick@natamus.com>
2023-12-12 11:02:42 +0100

    Automatic Doors source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/automaticdoors/util/Util.java
@@ -0,0 +1,47 @@
+package com.natamus.automaticdoors.util;
+
+import com.natamus.automaticdoors.config.ConfigHandler;
+import com.natamus.automaticdoors.events.DoorEvent;
+import com.natamus.collective.functions.TaskFunctions;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoorBlock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+	private static final List<BlockPos> runnables = new ArrayList<BlockPos>();
+
+	public static Boolean isDoor(Block block) {
+		if (block instanceof DoorBlock) {
+			if (!ConfigHandler.shouldOpenIronDoors) {
+				String name = block.toString().toLowerCase();
+				return !name.contains("iron");
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public static void delayDoorClose(Level level, BlockPos pos) {
+		if (pos == null) {
+			return;
+		}
+
+		if (runnables.contains(pos)) {
+			return;
+		}
+
+		runnables.add(pos);
+		if (!level.isClientSide) {
+			TaskFunctions.enqueueCollectiveTask(level.getServer(), () -> {
+				if (!DoorEvent.toclosedoors.get(level).contains(pos) && !DoorEvent.newclosedoors.get(level).contains(pos)) {
+					DoorEvent.newclosedoors.get(level).add(pos);
+				}
+				runnables.remove(pos);
+			}, (ConfigHandler.doorOpenTime/1000)*20);
+		}
+	}
+}
---
+++ Forge/src/main/java/com/natamus/automaticdoors/forge/events/ForgeDoorEvent.java
@@ -0,0 +1,36 @@
+package com.natamus.automaticdoors.forge.events;
+
+import com.natamus.automaticdoors.events.DoorEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.event.TickEvent.WorldTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeDoorEvent {
+	@SubscribeEvent
+	public void onWorldTick(WorldTickEvent e) {
+		Level level = e.world;
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		DoorEvent.onWorldTick((ServerLevel)level);
+	}
+
+	@SubscribeEvent
+	public void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.getCommandSenderWorld();
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		DoorEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
