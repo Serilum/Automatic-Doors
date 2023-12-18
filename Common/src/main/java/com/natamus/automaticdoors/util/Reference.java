d595c95ba94178734e9c5523e900549bd48ff7f8
Rick South <rick@natamus.com>
2023-12-18 10:42:44 +0100

    (v4.6) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/automaticdoors/util/Reference.java
+++ Common/src/main/java/com/natamus/automaticdoors/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.automaticdoors.util;
 public class Reference {
 	public static final String MOD_ID = "automaticdoors";
 	public static final String NAME = "Automatic Doors";
-	public static final String VERSION = "4.5";
+	public static final String VERSION = "4.6";
 	public static final String ACCEPTED_VERSIONS = "[1.20.2]";
 }
