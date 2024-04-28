09a343af534849ce5d609e91ae1741fbec4349f7
Rick South <rick@natamus.com>
2024-04-28 19:04:13 +0200

    (v4.7) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/automaticdoors/neoforge/events/NeoForgeDoorEvent.java
+++ NeoForge/src/main/java/com/natamus/automaticdoors/neoforge/events/NeoForgeDoorEvent.java
@@ -5,18 +5,17 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
-import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber
 public class NeoForgeDoorEvent {
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent e) {
-		Level level = e.level;
-		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+	public static void onWorldTick(LevelTickEvent.Pre e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
 			return;
 		}
 
@@ -24,10 +23,10 @@ public class NeoForgeDoorEvent {
 	}
 
 	@SubscribeEvent
-	public static void onPlayerTick(PlayerTickEvent e) {
-		Player player = e.player;
+	public static void onPlayerTick(PlayerTickEvent.Pre e) {
+		Player player = e.getEntity();
 		Level level = player.level();
-		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+		if (level.isClientSide) {
 			return;
 		}
 
