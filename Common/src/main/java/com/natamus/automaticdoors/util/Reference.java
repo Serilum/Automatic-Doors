b91b517c72e296b46ace2f250c23205a11f1fc61
Rick South <rick@natamus.com>
2024-10-24 20:15:01 +0200

    Automatic Doors source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/automaticdoors/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.automaticdoors.util;
+
+public class Reference {
+	public static final String MOD_ID = "automaticdoors";
+	public static final String NAME = "Automatic Doors";
+	public static final String VERSION = "4.8";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
