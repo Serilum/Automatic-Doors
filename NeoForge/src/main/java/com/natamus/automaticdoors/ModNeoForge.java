6e60b7056b623d4e1bf3c02427afac61d0b48d9e
Rick South <rick@natamus.com>
2025-10-08 01:51:02 +0200

    Automatic Doors source code for 1.21.10.

---
+++ NeoForge/src/main/java/com/natamus/automaticdoors/ModNeoForge.java
@@ -0,0 +1,39 @@
+package com.natamus.automaticdoors;
+
+import com.natamus.automaticdoors.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.automaticdoors.neoforge.events.NeoForgeDoorEvent;
+import com.natamus.automaticdoors.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeDoorEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
